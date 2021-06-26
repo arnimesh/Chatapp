8f29d80101d96f156cfffbe461d2d95d67569a2c
Arnimesh Kumar Singh <arnimeshk508@gmail.com>
2021-06-26 12:34:57 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+var express=require('express');
+const app=express();
+const http=require('http').createServer(app)
+
+
+const PORT=process.env.PORT||3000;
+app.use(express.static(__dirname+'/public'))
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html');
+})
+
+http.listen(PORT,()=>{
+    console.log(`listening on port ${PORT}`);
+})
+
+const io = require('socket.io')(http)
+
+io.on('connection', (socket) => {
+    console.log('Connected...')
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg)
+    })
+
+})
+
